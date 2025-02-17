2bfe64b62f1f4fc1442ba2d0af02799456ee066b
RyanSikandar <ryansikandar@gmail.com>
2025-02-17 23:01:12 +0500

    feat: echo command

--- app/main.js
+++ app/main.js
@@ -14,7 +14,13 @@ rl.prompt();
 //using the on method to listen for the line event
 rl.on("line", (input) => {
   if (input.startsWith("exit")) {
+    //If the input starts with exit, the program will exit
     exit(0)
+  }
+  //Implementing the Echo command
+  if (input.startsWith("echo")) {
+    //The echo command will print the input without the echo
+    console.log(input.slice(5));
   }
   console.log(`${input}: command not found`);
   rl.prompt();
