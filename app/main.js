f4838b96340345a7962eb1688b00e1a004d49d62
RyanSikandar <ryansikandar@gmail.com>
2025-02-18 23:50:53 +0500

    fix: how did i miss this stupid mistake

--- app/main.js
+++ app/main.js
@@ -13,9 +13,8 @@ rl.prompt();
 
 const checkType = (input) => {
   const type = input.split(" ")[1];
-  if (type === 'echo' | 'exit' | 'type') {
+  if (type === 'echo' ||type=== 'exit' ||type=== 'type') {
     console.log(`${type} is a shell builtin`);
-
     rl.prompt();
   } else {
     console.log(`${type}: not found`);
