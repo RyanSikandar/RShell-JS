3d79bc0c4ff0d4de034120a3d2e6657585efbec3
RyanSikandar <ryansikandar@gmail.com>
2025-02-21 19:33:12 +0500

    feat: backslash inside quotes

--- app/main.js
+++ app/main.js
@@ -27,9 +27,9 @@ function handleEcho(input) {
   let inDoubleQuotes = false;
 
   for (let i = 0; i < input.length; i++) {
-//Handling each character in the input
+    //Handling each character in the input
     const char = input[i];
-    
+
     if (char === '"' && !inSingleQuotes) {
 
       inDoubleQuotes = !inDoubleQuotes;
@@ -48,124 +48,141 @@ function handleEcho(input) {
       }
 
     } else {
-      //If the character is not a space or single or double quote, we add it to the current word
-      currentArg += char;
+      //For quoted backslashes
+      if (char === '\\' && (inSingleQuotes || inDoubleQuotes)) {
+        if (input[i + 1] === '\\') {
+          currentArg += '\\';
+          i++;
+        }
+        else {
+          currentArg += char;
+        }
 
-    }
-
-  }
-
-  //If the current word is not empty, we add it to the args array
-
-  if (currentArg.length > 0) {
-
-    args.push(currentArg);
-
-  }
-
-  let cmd = args[0];
-
-  //Remove the first word from the array
-  args.shift();
-
-  console.log(args.join(' '));
-}
-
-//Function to change the directory
-function changeDirectory(path) {
-  try {
-    if (path === `~`) {
-      chdir(home);
-    }
-    else { chdir(path); }
-
-  } catch (err) {
-    console.error(`cd: ${path}: No such file or directory`);
-  }
-}
-
-//Function to execute the executable file (eg: ls, cat, etc)
-function executeFile(input) {
-  const [command, ...args] = input.match(/'[^']*'|"[^"]*"|\S+/g)?.map(arg => arg.replace(/^['"]|['"]$/g, '')) || [];
-
-  const path = process.env.PATH.split(":");
-  let valid = false;
-
-  if (command === "cd") {
-    changeDirectory(args[0]);
-    return;
-  }
-
-  path.forEach((path) => {
-    const commandPath = path + "/" + command;
-    if (!valid && fs.existsSync(commandPath) && fs.statSync(commandPath).isFile()) {
-      valid = true;
-      try {
-        execFileSync(command, args, { encoding: 'utf-8', stdio: 'inherit' });
-      } catch (err) {
-        console.error(`Error executing ${command}:`, err.message);
       }
+      //For unquoted backslashes
+      else if (char === '\\' && !(inSingleQuotes || inDoubleQuotes)) {
+       //Preserve the literal value of the next character
+        currentArg += input[i + 1];
+        i++;
+      }
+      else {
+        //If the character is not a space or single or double quote, we add it to the current words
+        currentArg += char;
+      }}
+
     }
-  });
-  if (!valid) {
-    console.log(`${command}: command not found`);
-  }
-}
 
+    //If the current word is not empty, we add it to the args array
 
-//Function to find the path of the executable file in the PATH
-const findPath = (type) => {
-  const paths = process.env.PATH.split(":");
-  let found = false;
-  paths.forEach((path) => {
-    const commandPath = path + "/" + type;
-    if (!found && fs.existsSync(commandPath)) {
-      console.log(`${type} is ${commandPath}`);
-      found = true;
+    if (currentArg.length > 0) {
+
+      args.push(currentArg);
+
     }
-  });
-  if (!found) {
-    console.log(`${type}: not found`);
-  }
-}
 
-//Function to check the type of the command and print the result
-const types = ['echo', 'exit', 'type', 'pwd', 'cd'];
-const checkType = (input) => {
-  const type = input.split(" ")[1];
-  if (types.includes(type)) {
-    console.log(`${type} is a shell builtin`);
-    rl.prompt();
-  } else {
-    findPath(type);
-    rl.prompt();
-  }
-}
+    let cmd = args[0];
 
-//using the on method to listen for the line event
-rl.on("line", (input) => {
-  if (input.startsWith("type")) {
-    checkType(input);
-  }
-  else if (input.startsWith("pwd")) {
-    //Prints the current working directory for the process
-    console.log(process.cwd());
-    rl.prompt();
-  }
-  else if (input.startsWith("exit")) {
-    //If the input starts with exit, the program will exit
-    exit(0)
+    //Remove the first word from the array
+    args.shift();
 
+    console.log(args.join(' '));
   }
-  //Implementing the Echo command
-  else if (input.includes("echo")) {
-    //The echo command will print the input without the echo    
-    handleEcho(input);
-    rl.prompt();
+
+  //Function to change the directory
+  function changeDirectory(path) {
+    try {
+      if (path === `~`) {
+        chdir(home);
+      }
+      else { chdir(path); }
+
+    } catch (err) {
+      console.error(`cd: ${path}: No such file or directory`);
+    }
   }
-  else {
-    //Executes the file input that we have given the shell. Detects if it is a wrong input as well.
-    executeFile(input);
-    rl.prompt();
+
+  //Function to execute the executable file (eg: ls, cat, etc)
+  function executeFile(input) {
+    const [command, ...args] = input.match(/'[^']*'|"[^"]*"|\S+/g)?.map(arg => arg.replace(/^['"]|['"]$/g, '')) || [];
+
+    const path = process.env.PATH.split(":");
+    let valid = false;
+
+    if (command === "cd") {
+      changeDirectory(args[0]);
+      return;
+    }
+
+    path.forEach((path) => {
+      const commandPath = path + "/" + command;
+      if (!valid && fs.existsSync(commandPath) && fs.statSync(commandPath).isFile()) {
+        valid = true;
+        try {
+          execFileSync(command, args, { encoding: 'utf-8', stdio: 'inherit' });
+        } catch (err) {
+          console.error(`Error executing ${command}:`, err.message);
+        }
+      }
+    });
+    if (!valid) {
+      console.log(`${command}: command not found`);
+    }
   }
-})
+
+
+  //Function to find the path of the executable file in the PATH
+  const findPath = (type) => {
+    const paths = process.env.PATH.split(":");
+    let found = false;
+    paths.forEach((path) => {
+      const commandPath = path + "/" + type;
+      if (!found && fs.existsSync(commandPath)) {
+        console.log(`${type} is ${commandPath}`);
+        found = true;
+      }
+    });
+    if (!found) {
+      console.log(`${type}: not found`);
+    }
+  }
+
+  //Function to check the type of the command and print the result
+  const types = ['echo', 'exit', 'type', 'pwd', 'cd'];
+  const checkType = (input) => {
+    const type = input.split(" ")[1];
+    if (types.includes(type)) {
+      console.log(`${type} is a shell builtin`);
+      rl.prompt();
+    } else {
+      findPath(type);
+      rl.prompt();
+    }
+  }
+
+  //using the on method to listen for the line event
+  rl.on("line", (input) => {
+    if (input.startsWith("type")) {
+      checkType(input);
+    }
+    else if (input.startsWith("pwd")) {
+      //Prints the current working directory for the process
+      console.log(process.cwd());
+      rl.prompt();
+    }
+    else if (input.startsWith("exit")) {
+      //If the input starts with exit, the program will exit
+      exit(0)
+
+    }
+    //Implementing the Echo command
+    else if (input.includes("echo")) {
+      //The echo command will print the input without the echo    
+      handleEcho(input);
+      rl.prompt();
+    }
+    else {
+      //Executes the file input that we have given the shell. Detects if it is a wrong input as well.
+      executeFile(input);
+      rl.prompt();
+    }
+  })
