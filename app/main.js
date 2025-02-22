e773eadc83903efafbd1cf05907e5856a1a1d298
RyanSikandar <ryansikandar@gmail.com>
2025-02-22 20:07:01 +0500

    fix

--- app/main.js
+++ app/main.js
@@ -107,7 +107,7 @@ function handleEcho(input) {
           fs.mkdirSync(path.dirname(file), { recursive: true });
       
           // Write error output
-          console.error(args.join(' '));
+          fs.writeFileSync(file, args.join(' '));
         } catch (err) {
           console.error(`echo: ${file}: No such file or directory`);
         }
